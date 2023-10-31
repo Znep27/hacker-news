3c5a44c71a475fdaebd325d0242b664cebf0c3b3
Marc Rozenburgh <znep27@hotmail.com>
2023-10-31 16:35:54 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  const updateArticles = async (e) => {
+    console.log('e.target.value,', e.target.value)
+    const { data } = await axios.get(`http://hn.algolia.com/api/v1/search?query=${e.target.value}`)
+    console.log(data.hits)
+    setArticles(data.hits)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input onChange={updateArticles} />
+      </form>
+      <div>
+        {articles.map((article, i) => <p key={i}>{article.title}</p>)}
+      </div>
     </div>
   );
 }
